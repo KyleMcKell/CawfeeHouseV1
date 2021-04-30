48cf1ec73b54349bc92392137a97e2147f54b94f
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-04-30 01:17:27 -0600

    created login function for logging in a user, and also a get all users function

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -30,10 +30,10 @@ const register = (req: Request, res: Response, next: NextFunction) => {
 		}
 		try {
 			let query =
-				'INTO INTO Users (username, email, password) VALUES($1, $2, $3) RETURNING *';
+				'INTO INTO users (username, email, hashed_pw) VALUES($1, $2, $3) RETURNING *';
 
 			const newUser = await pg.query(query, [username, email, hash]);
-			res.json(newUser.rows[0]);
+			res.json(newUser);
 		} catch (error) {
 			logging.error(NAMESPACE, error.message, error);
 
@@ -46,9 +46,65 @@ const register = (req: Request, res: Response, next: NextFunction) => {
 };
 
 //$ Login user and return token and user object
-const login = (req: Request, res: Response, next: NextFunction) => {};
+const login = async (req: Request, res: Response, next: NextFunction) => {
+	let { username, password } = req.body;
+
+	try {
+		let query = `SELECT * FROM users WHERE username = ${username}`;
+
+		const users = await pg.query<IUser>(query);
+
+		bcryptjs.compare(password, users.rows[0].password, (error, result) => {
+			if (error) {
+				return res.status(401).json({
+					message: 'Password Mismatch',
+				});
+			} else if (result) {
+				signJWT(users.rows[0], (error, token) => {
+					if (error) {
+						return res.status(401).json({
+							message: 'Unable to Sign JWT',
+							error,
+						});
+					} else if (token) {
+						return res.status(200).json({
+							message: 'Auth Successful',
+							token,
+							user: users.rows[0],
+						});
+					}
+				});
+			}
+		});
+	} catch (error) {
+		logging.error(NAMESPACE, error.message, error);
+
+		return res.status(500).json({
+			message: error.message,
+			error,
+		});
+	}
+};
 
 //$ Return all users in database without passwords
-const getAllUsers = (req: Request, res: Response, next: NextFunction) => {};
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let query = `SELECT id, username FROM users`;
+
+		const users = await pg.query<IUser>(query);
+
+		return res.status(200).json({
+			users,
+			count: users.rows.length,
+		});
+	} catch (error) {
+		logging.error(NAMESPACE, error.message, error);
+
+		return res.status(500).json({
+			message: error.message,
+			error,
+		});
+	}
+};
 
 export default { validateToken, register, login, getAllUsers };
